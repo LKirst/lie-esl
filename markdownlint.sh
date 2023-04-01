2eef5b9185e64be2f3d331ad0b692a15dfbbfcc7
LKirst <lk_tech@mailbox.org>
2023-04-01 19:20:59 +0200

    Call pymarkdown with strict-config

--- markdownlint.sh
+++ markdownlint.sh
@@ -1,3 +1,1 @@
-# MD003 is violated because the end of the yaml front matter is parsed as a heading
-# MD041 is violated because due to the yaml front matter, the first line is not a heading
-pymarkdown -d MD003,MD041 scan . ./*/*
+pymarkdown --strict-config -s extensions-front-matter.enabled=$!True scan test.md
