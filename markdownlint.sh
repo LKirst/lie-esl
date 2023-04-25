d1e1688b0cd4ad44dc60b9b4b06ca91829322791
LKirst <lk_tech@mailbox.org>
2023-04-25 12:15:18 +0200

    Correctly activate the yaml extension

--- markdownlint.sh
+++ markdownlint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-pymarkdown --strict-config --disable-rules md033 -s extensions-front-matter.enabled=$!True scan . -r
+pymarkdown --strict-config --disable-rules md033 -s extensions.front-matter.enabled=\$!True scan . -r
